37dc2872ec47d40be762b1313f71c01cf2011c64
vaishalisingh102005-coder <vaishalisingh102005@gmail.com>
2026-07-20 21:11:35 +0530

    Time: 0 ms (100%), Space: 8.1 MB (40.03%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        //string ans="";
+        string n=to_string(num);
+        for(int i=0;i<n.size();i++){
+            if(n[i]=='6'){
+                n[i]='9';
+                break;
+            }
+        }
+        int ans=stoi(n);
+        return ans;
+    }
+};
