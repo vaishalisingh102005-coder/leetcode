8178c2125f75d6389dbf57e9d885519fae4c2b38
vaishalisingh102005-coder <vaishalisingh102005@gmail.com>
2026-09-14 21:39:42 +0530

    Time: 0 ms (100%), Space: 8 MB (83.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -4,12 +4,14 @@ public:
         if(s.length()!=goal.length()){
             return false;
         }
-        for(int i=0;i<s.size();i++){
-            string rot=s.substr(i)+s.substr(0,i);
-            if(rot==goal){
-                return true;
-            }
-        }
+        // for(int i=0;i<s.size();i++){
+        //     string rot=s.substr(i)+s.substr(0,i);
+        //     if(rot==goal){
+        //         return true;
+        //     }
+        // }
+        string doubledS = s + s;
+        return doubledS.find(goal) != string::npos;
         return false;
     }
 };
