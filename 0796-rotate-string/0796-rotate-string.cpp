39b4e2cd08cff757180025c38b581079f62f14be
vaishalisingh102005-coder <vaishalisingh102005@gmail.com>
2026-09-14 21:38:32 +0530

    Time: 2 ms (8.63%), Space: 8.2 MB (6.22%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        for(int i=0;i<s.size();i++){
+            string rot=s.substr(i)+s.substr(0,i);
+            if(rot==goal){
+                return true;
+            }
+        }
+        return false;
+    }
+};
